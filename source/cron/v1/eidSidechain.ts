1792fc98ddb299284d79aab9c6bdb5f239b2b83f
Kiran Pachhai <kiran.pachhai@gmail.com>
2021-08-12 16:45:30 -0400

    Added user stats collection as part of daily cron job

--- source/cron/v1/eidSidechain.ts
+++ source/cron/v1/eidSidechain.ts
@@ -94,12 +94,12 @@ function publishDIDTx(network: string) {
                                     });
                                 }
                             })
-                            .catch((err) => {
-                                logging.error(NAMESPACE, 'Error while publishing the a pending DID transaction to the blockchain: ', err);
+                            .catch((error) => {
+                                logging.error(NAMESPACE, 'Error while publishing the a pending DID transaction to the blockchain: ', error);
 
                                 didTx.status = config.txStatus.cancelled;
                                 didTx.extraInfo = {
-                                    error: err
+                                    error
                                 };
                                 didTx.save();
                             });
@@ -157,24 +157,49 @@ function dailyCronjob(network: string) {
     // TODO: Uncomment this when going to production
     //cron.schedule('0 0 * * * *', () => {
 
-    cron.schedule('*/5 * * * * *', () => {
+    cron.schedule('*/5 * * * * *', async () => {
         logging.info(NAMESPACE, `Started cronjob: dailyCronjob: ${network}`);
 
-        rpcService.getBalance(network, config.blockchain.eidSidechain.wallets.keystores[0].address).then((balanceResponse) => {
-            console.log(balanceResponse);
-        });
+        const today = new Date('2021-08-05');
 
-        const subject = 'Assist Service Daily Stats';
-        const html = '<table><tr><th>Address</th><th>Balance</th><th>Type</th></tr>';
+        // ---------------------------------------------------------------------------------------
 
-        //sendNotification.sendEmail(subject, config.smtpCreds.sender, html);
-
+        // Reset the user accounts for today and get some general stats
         const conn = network === config.blockchain.testnet ? connTestnet : connMainnet;
+        let generalUserStats: any = {
+            free: {
+                today: 0,
+                all: 0
+            },
+            premium: {
+                today: 0,
+                all: 0
+            },
+            usersToday: {},
+            numUsersToday: 0,
+            numUsersAllTime: 0
+        };
         conn.models.User.find()
             .select('-password')
             .exec()
             .then((users) => {
                 users.map((user) => {
+                    let freeEndpointsAll = user.requests.freeEndpoints.all;
+                    let premiumEndpointsAll = user.requests.premiumEndpoints.all;
+                    generalUserStats.free.today += user.requests.freeEndpoints.today;
+                    generalUserStats.free.all += freeEndpointsAll;
+                    generalUserStats.premium.today += user.requests.premiumEndpoints.today;
+                    generalUserStats.premium.all += premiumEndpointsAll;
+                    generalUserStats.numUsersAllTime += 1;
+                    if (new Date(user.createdAt) >= today) {
+                        generalUserStats.numUsersToday += 1;
+                        generalUserStats.usersToday[user.username] = {
+                            free: freeEndpointsAll,
+                            premium: premiumEndpointsAll
+                        };
+                    }
+
+                    // Reset
                     user.requests.freeEndpoints.today = 0;
                     user.requests.premiumEndpoints.today = 0;
                     user.save();
@@ -183,6 +208,95 @@ function dailyCronjob(network: string) {
             .catch((err) => {
                 logging.error(NAMESPACE, 'Error while trying to reset daily limit for the users: ', err);
             });
+        // Get general user stats
+        const getGeneralUserStats = async () => {
+            let generalUserStatsHtml: string = `<table><tr><th></th><th>Today</th><th>All time</th></tr>`;
+            generalUserStatsHtml += `<tr><th>Number of Users</th><th>${generalUserStats.numUsersToday}</th><th>${generalUserStats.numUsersAllTime}</th></tr></table><br>`;
+
+            generalUserStatsHtml += `<table><tr><th>Request Type</th><th>Today</th><th>All time</th></tr>`;
+            generalUserStatsHtml += `<tr><td>Free</td><td>${generalUserStats.free.today}</td><td>${generalUserStats.free.all}</td></tr>`;
+            generalUserStatsHtml += `<tr><td>Premium</td><td>${generalUserStats.premium.today}</td><td>${generalUserStats.premium.all}</td></tr></table><br>`;
+
+            generalUserStatsHtml += `<table><tr><th>New User</th><th>Free</th><th>Premium</th></tr>`;
+            for (let username in generalUserStats.usersToday) {
+                generalUserStatsHtml += `<tr><td>${username}</td><td>${generalUserStats.usersToday[username].free}</td><td>${generalUserStats.usersToday[username].premium}</td></tr>`;
+            }
+            generalUserStatsHtml += `</table>`;
+
+            return generalUserStatsHtml;
+        };
+
+        // ---------------------------------------------------------------------------------------
+
+        // Get wallet stats
+        const getWalletStats = async () => {
+            let walletStats: string = '<table><tr><th>Address</th><th>Balance</th><th>Type</th></tr>';
+
+            let testAddress = '0x365b70f14e10b02bef7e463eca6aa3e75ca3cdb1';
+            let testBalance = await rpcService.getBalance(network, testAddress).then((balanceResponse) => {
+                if (balanceResponse.meta.message === 'OK') {
+                    return balanceResponse.data.value;
+                } else {
+                    logging.error(NAMESPACE, `Error while getting balance of '${testAddress}': `, balanceResponse.error);
+                    return 'ERR';
+                }
+            });
+            walletStats += `<tr><td>${testAddress}</td><td>${testBalance}</td><td>Testing</td></tr>`;
+
+            for (let keystore of config.blockchain.eidSidechain.wallets.keystores) {
+                let address = `0x${keystore.address}`;
+                let balance = await rpcService.getBalance(network, address).then((balanceResponse) => {
+                    if (balanceResponse.meta.message === 'OK') {
+                        return balanceResponse.data.value;
+                    } else {
+                        logging.error(NAMESPACE, `Error while getting balance of '${address}': `, balanceResponse.error);
+                        return 'ERR';
+                    }
+                });
+                walletStats += `<tr><td>${address}</td><td>${balance}</td><td>Production</td></tr>`;
+            }
+            walletStats += '</table>';
+            return walletStats;
+        };
+        // ---------------------------------------------------------------------------------------
+
+        let htmlContent: string = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <style>
+                    table {
+                        font-family: arial, sans-serif;
+                        border-collapse: collapse;
+                        width: 100%;
+                    }
+
+                    td, th {
+                        border: 1px solid #dddddd;
+                        text-align: left;
+                        padding: 8px;
+                    }
+
+                    tr:nth-child(even) {
+                        background-color: #dddddd;
+                    }
+                </style>
+            </head>
+            <body>          
+                <h2>Wallets and Current Balances</h2>
+                ${await getWalletStats()}
+                <h2>General User Stats</h2>
+                ${await getGeneralUserStats()}
+                <h2>DID Transactions</h2>
+                {didtx_stats}
+                <h2>Cancelled Transactions</h2>
+                {quarantined_transactions}
+            </body>
+        </html>
+        `;
+
+        const subject: string = `Assist Service Daily Stats - ${network}`;
+        sendNotification.sendEmail(subject, config.smtpCreds.sender, htmlContent);
 
         logging.info(NAMESPACE, `Completed cronjob: dailyCronjob: ${network}`);
     });
